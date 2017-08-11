a8b0fd27bf961ac0648ad730df8a776bd333168b
baoyas <645131056@qq.com>
2017-08-11 15:37:21 +0800

    zhu si

--- angular-tour-of-heroes/src/app/in-memory-data.service.ts
+++ angular-tour-of-heroes/src/app/in-memory-data.service.ts
@@ -3,9 +3,9 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     let heroes = [
-      {id: 11, name: 'Mr. Nice'},
-      {id: 12, name: 'Narco'},
-      {id: 13, name: 'Bombasto'},
+      {id: 11, name: 'Mr. Right'},
+      {id: 12, name: 'windstorm'},
+      {id: 13, name: 'petter'},
       {id: 14, name: 'Celeritas'},
       {id: 15, name: 'Magneta'},
       {id: 16, name: 'RubberMan'},
