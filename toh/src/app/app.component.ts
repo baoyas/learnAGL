3367ff1fe4d9c769bc1c278653b9528c3f01caff
baoyas <645131056@qq.com>
2017-08-24 16:26:41 +0800

    toh-2

--- toh/src/app/app.component.ts
+++ toh/src/app/app.component.ts
@@ -4,15 +4,29 @@ export class Hero{
 	id : number;
 	name : string;
 }
+const HEROES: Hero[] = [
+  { id: 1, name: '寒冰' },
+  { id: 2, name: '老牛' },
+  { id: 3, name: '妖姬' },
+  { id: 4, name: '阿狸' },
+  { id: 5, name: '死歌' },
+  { id: 6, name: '阿卡丽' },
+  { id: 7, name: '武器' },
+  { id: 8, name: '船长' },
+  { id: 9, name: 'VN' },
+  { id: 10, name: '小炮' }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'baoyas-英雄之路';
-  hero : Hero = {
-  	id : 1,
-  	name : "风暴之子"
+	title = 'baoyas-英雄之路';
+	heroes = HEROES;
+	selectedHero : Hero;
+  onSelect(hero: Hero): void {
+  	this.selectedHero = hero;
   };
 }
