e9ad66d48c178f2da7f291d801d9be857ac18744
baoyas <645131056@qq.com>
2017-08-24 17:48:45 +0800

    toh4:service

--- toh/src/app/app.component.ts
+++ toh/src/app/app.component.ts
@@ -1,28 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
-const HEROES: Hero[] = [
-  { id: 1, name: '寒冰' },
-  { id: 2, name: '老牛' },
-  { id: 3, name: '妖姬' },
-  { id: 4, name: '阿狸' },
-  { id: 5, name: '死歌' },
-  { id: 6, name: '阿卡丽' },
-  { id: 7, name: '武器' },
-  { id: 8, name: '船长' },
-  { id: 9, name: 'VN' },
-  { id: 10, name: '小炮' }
-];
-
+import { HeroService } from './hero.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+	constructor(private heroService: HeroService){
+		
+	}
+	ngOnInit(): void {
+		this.getHeroes();
+  }
 	title = 'baoyas-英雄之路';
-	heroes = HEROES;
 	selectedHero : Hero;
+	heroes : Hero[];
   onSelect(hero: Hero): void {
   	this.selectedHero = hero;
   };
+  getHeroes():void{
+  	this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
 }
---
+++ toh/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: '寒冰' },
+  { id: 2, name: '老牛' },
+  { id: 3, name: '妖姬' },
+  { id: 4, name: '阿狸' },
+  { id: 5, name: '死歌' },
+  { id: 6, name: '阿卡丽' },
+  { id: 7, name: '武器' },
+  { id: 8, name: '船长' },
+  { id: 9, name: 'VN' },
+  { id: 10, name: '小炮' }
+];
